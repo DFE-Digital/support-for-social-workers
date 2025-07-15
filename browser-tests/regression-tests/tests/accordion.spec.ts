5e1cbdbb09b661c8154b97f036e8ac78a6bc68c6
Matt Barton <matt.barton@hippodigital.co.uk>
2025-07-15 09:07:54 +0100

    WiP: accordion regression tests in progress

--- browser-tests/regression-tests/tests/accordion.spec.ts
+++ browser-tests/regression-tests/tests/accordion.spec.ts
@@ -3,11 +3,25 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Accordion', () => {
 
+    let elements = [];
+
     test.beforeEach(async ({ page }) => {
         await page.goto('/demo/accordion');
+        elements['accordionContainer'] = page.getByTestId('accordion');
+        elements['accordionSections'] = await elements['accordionContainer'].getByTestId('accordion-section');
+
     });
 
-    test('Margins, Padding, and Borders', async ({ page }) => {
+    test('Component structure', async ({ page }) => {
+
+        await expect(elements['accordionContainer']).toBeVisible();
+        await expect(elements['accordionSections']).toHaveCount(2);
+    });
+
+    test('Margins, Padding, Borders, Styles', async ({ page }) => {
+        await expect(elements['accordionContainer']).toHaveClass('govuk-accordion');
+        await expect(elements['accordionContainer']).toHaveCSS('margin-bottom', '30px');
+        await expect(elements['accordionContainer']).toHaveCSS('border-bottom', '1px solid rgb(177, 180, 182)');
     });
 
     test('Click events', async ({ page }) => {
