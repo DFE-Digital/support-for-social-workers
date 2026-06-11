c2912ddb905df9be4906aa110bb7e032e9405bc8
charliedeeley <Charlie.DEELEY@education.gov.uk>
2026-06-11 14:32:38 +0100

    fix: update back-to-top to use let instead of var

--- Childrens-Social-Care-CPD/wwwroot/javascript/components/back-to-top.js
+++ Childrens-Social-Care-CPD/wwwroot/javascript/components/back-to-top.js
@@ -139,9 +139,9 @@ ContentsListWithBody.prototype.checkScroll = function () {
 ContentsListWithBody.prototype.updateVisibility = function () {
     if (this.disabled) return this.hide();
 
-    var isPastStart = this.startPosition < this.windowVerticalPosition;
+    let isPastStart = this.startPosition < this.windowVerticalPosition;
     if (isPastStart) {
-        var isPastEnd = this.stopPosition < this.windowVerticalPosition;
+        let isPastEnd = this.stopPosition < this.windowVerticalPosition;
         if (isPastEnd) {
             this.hide();
         } else {
@@ -169,7 +169,7 @@ ContentsListWithBody.prototype.destroy = function () {
 };
 
 document.addEventListener('DOMContentLoaded', function () {
-    var element = document.body.querySelector("[data-module='contents-list-with-body']");
+    let element = document.body.querySelector("[data-module='contents-list-with-body']");
     if (element) {
         new ContentsListWithBody(element).init();
     }
