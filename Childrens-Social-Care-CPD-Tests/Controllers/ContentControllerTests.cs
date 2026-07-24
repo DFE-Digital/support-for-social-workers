782a892a578556473fc41f568cda4c5ea9c436d5
Matt Barton <matt.barton@hippodigital.co.uk>
2026-07-24 14:15:28 +0100

    fix: temp change test assertion

--- Childrens-Social-Care-CPD-Tests/Controllers/ContentControllerTests.cs
+++ Childrens-Social-Care-CPD-Tests/Controllers/ContentControllerTests.cs
@@ -69,7 +69,7 @@ public class ContentControllerTests
         var actual = await _contentController.Index("home");
 
         // assert
-        actual.Should().BeOfType<NotFoundResult>();
+        actual.Should().BeOfType<BadRequestResult>();
     }
 
     [Test]
